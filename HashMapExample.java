911ef08b57a2be22e200a8d42828494873fec2a8
Manpreet1011 <mmanna77@gmail.com>
2022-03-24 11:27:07 -0400

    Hash

---
+++ HashMapExample.java
@@ -0,0 +1,11 @@
+import java.util.HashMap;
+
+public class HashMapExample {
+    public static void main(String[] args) {
+        HashMap<String,String> names=new HashMap<>();
+        names.put("Manpreet","Punjab");
+        names.put("Abhay","Moga");
+        names.put("Param","Kot");
+        System.out.println(names);
+    }
+}
